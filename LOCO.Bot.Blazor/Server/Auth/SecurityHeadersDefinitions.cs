0cfe3070ef7baeb881624789d6f152c11911dcbb
Q-Sharp <8755388+Q-Sharp@users.noreply.github.com>
2022-04-18 23:55:56 +0200

    Update security headers

--- LOCO.Bot.Blazor/Server/Auth/SecurityHeadersDefinitions.cs
+++ LOCO.Bot.Blazor/Server/Auth/SecurityHeadersDefinitions.cs
@@ -15,20 +15,34 @@ public static class SecurityHeadersDefinitions
                 {
                     builder.AddObjectSrc().None();
                     builder.AddBlockAllMixedContent();
-                    builder.AddImgSrc().Self().From("data:");
+                    builder.AddImgSrc()
+                           .Self()
+                           .From("https://locowheel.mmbot.xyz/")
+                           .From("data:");
                     builder.AddFormAction().Self().From(idpHost);
-                    builder.AddFontSrc().Self().OverHttps();
+                    builder.AddFontSrc()
+                           .Self()
+                           .From("https://fonts.googleapis.com")
+                           .From("https://locowheel.mmbot.xyz/")
+                           .OverHttps();
                     builder.AddBaseUri().Self();
                     builder.AddFrameAncestors().None();
 
                     if (!isDev)
                     {
-                        builder.AddStyleSrc().Self();
+                        builder.AddStyleSrc()
+                               .Self()
+                               .From("https://ajax.googleapis.com/")
+                               .From("https://static.cloudflareinsights.com/")
+                               .From("https://locowheel.mmbot.xyz/");
 
                         // due to Blazor
                         builder.AddScriptSrc()
                                .Self()
                                .WithHash256("v8v3RKRPmN4odZ1CWM5gw80QKPCCWMcpNeOmimNL2AA=")
+                               .From("https://ajax.googleapis.com/")
+                               .From("https://static.cloudflareinsights.com/")
+                               .From("https://locowheel.mmbot.xyz/")
                                .UnsafeEval();
                     }
 
