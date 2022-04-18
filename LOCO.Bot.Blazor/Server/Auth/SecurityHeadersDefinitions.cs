c256a783764bbe3a4cd4efa3fb4f1e237360e78e
Q-Sharp <8755388+Q-Sharp@users.noreply.github.com>
2022-04-19 00:11:50 +0200

    ujhgf

--- LOCO.Bot.Blazor/Server/Auth/SecurityHeadersDefinitions.cs
+++ LOCO.Bot.Blazor/Server/Auth/SecurityHeadersDefinitions.cs
@@ -11,44 +11,44 @@ public static class SecurityHeadersDefinitions
                 .AddReferrerPolicyStrictOriginWhenCrossOrigin()
                 .AddCrossOriginOpenerPolicy(builder => builder.SameOrigin())
                 .AddCrossOriginResourcePolicy(builder => builder.SameOrigin())
-                .AddContentSecurityPolicy(builder =>
-                {
-                    builder.AddObjectSrc().None();
-                    builder.AddBlockAllMixedContent();
-                    builder.AddImgSrc()
-                           .Self()
-                           .From("https://locowheel.mmbot.xyz")
-                           .From("data:");
-                    builder.AddFormAction().Self().From(idpHost);
-                    builder.AddFontSrc()
-                           .Self()
-                           .From("https://fonts.googleapis.com")
-                           .From("https://locowheel.mmbot.xyz")
-                           .OverHttps();
-                    builder.AddBaseUri().Self();
-                    builder.AddFrameAncestors().None();
+                //.AddContentSecurityPolicy(builder =>
+                //{
+                //    builder.AddObjectSrc().None();
+                //    builder.AddBlockAllMixedContent();
+                //    builder.AddImgSrc()
+                //           .Self()
+                //           .From("https://locowheel.mmbot.xyz")
+                //           .From("data:");
+                //    builder.AddFormAction().Self().From(idpHost);
+                //    builder.AddFontSrc()
+                //           .Self()
+                //           .From("https://fonts.googleapis.com")
+                //           .From("https://locowheel.mmbot.xyz")
+                //           .OverHttps();
+                //    builder.AddBaseUri().Self();
+                //    builder.AddFrameAncestors().None();
 
-                    if (!isDev)
-                    {
-                        builder.AddStyleSrc()
-                               .Self()
-                               .From("https://static.cloudflareinsights.com")
-                               .From("https://locowheel.mmbot.xyz");
+                //    if (!isDev)
+                //    {
+                //        builder.AddStyleSrc()
+                //               .Self()
+                //               .From("https://static.cloudflareinsights.com")
+                //               .From("https://locowheel.mmbot.xyz");
 
-                        // due to Blazor
-                        builder.AddScriptSrc()
-                               .Self()
-                               .WithHash256("v8v3RKRPmN4odZ1CWM5gw80QKPCCWMcpNeOmimNL2AA=")
-                               .From("https://ajax.googleapis.com/")
-                               .From("https://static.cloudflareinsights.com/")
-                               .From("https://locowheel.mmbot.xyz/")
-                               .UnsafeEval();
-                    }
+                //        // due to Blazor
+                //        builder.AddScriptSrc()
+                //               .Self()
+                //               .WithHash256("v8v3RKRPmN4odZ1CWM5gw80QKPCCWMcpNeOmimNL2AA=")
+                //               .From("https://ajax.googleapis.com/")
+                //               .From("https://static.cloudflareinsights.com/")
+                //               .From("https://locowheel.mmbot.xyz/")
+                //               .UnsafeEval();
+                //    }
 
-                    // disable script and style CSP protection if using Blazor hot reload
-                    // if using hot reload, DO NOT deploy with an insecure CSP
-                })
-                .RemoveServerHeader()
+                //    // disable script and style CSP protection if using Blazor hot reload
+                //    // if using hot reload, DO NOT deploy with an insecure CSP
+                //})
+                //.RemoveServerHeader()
                 .AddPermissionsPolicy(builder =>
                 {
                     builder.AddAccelerometer().None();
