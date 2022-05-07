5406bba2459ea553eaaa0025a3595fe4f89d1556
Q-Sharp <8755388+Q-Sharp@users.noreply.github.com>
2022-05-07 09:31:34 +0200

    div & canvas sollution

---
+++ LOCO.Bot.Web.Client/wwwroot/canvas.js
@@ -0,0 +1,20 @@
+﻿function renderJS(timeStamp) {
+    theInstance.invokeMethodAsync('RenderInBlazor', timeStamp);
+    window.requestAnimationFrame(renderJS);
+}
+
+function resizeCanvasToFitWindow() {
+    var page = document.getElementById('pageContainer');
+    if (page) {
+        page.width = window.innerWidth;
+        page.height = window.innerHeight;
+        theInstance.invokeMethodAsync('ResizeInBlazor', page.width, page.height);
+    }
+}
+
+window.initRenderJS = (instance) => {
+    window.theInstance = instance;
+    window.addEventListener("resize", resizeCanvasToFitWindow);
+    resizeCanvasToFitWindow();
+    window.requestAnimationFrame(renderJS);
+};
